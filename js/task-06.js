8fba6473edb18143c47ca6a7948e38efc7a89983
Volodymyr Horpynchenko <115591313+blowmybow@users.noreply.github.com>
2023-03-18 23:04:37 +0200

    task-06.js finished

---
+++ js/task-06.js
@@ -0,0 +1,14 @@
+const inputEl = document.getElementById("validation-input");
+
+inputEl.addEventListener("blur", onInputBlur);
+
+function onInputBlur() {
+    if (inputEl.value.length < inputEl.getAttribute("data-length") || 
+    inputEl.value.length > inputEl.getAttribute("data-length")) {
+        inputEl.classList.add("invalid")
+    } else {
+    inputEl.classList.remove("invalid")
+    inputEl.classList.add("valid")
+    }
+}
+    
