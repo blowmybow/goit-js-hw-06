3ee1d1553127ebcdd314c2f51c1a8dfe1768cf9e
Volodymyr Horpynchenko <115591313+blowmybow@users.noreply.github.com>
2023-03-19 11:02:57 +0200

    task-09.js finished

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,17 @@
+const refs = {
+  container: document.querySelector('.widget'),
+  span: document.querySelector('.color'),
+  button: document.querySelector('.change-color'),
+};
+
+refs.button.addEventListener('click', () => {
+  let hexColor = getRandomHexColor();
+  document.body.style.backgroundColor = hexColor;
+  refs.span.textContent = hexColor;
+});
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
-}
+};
